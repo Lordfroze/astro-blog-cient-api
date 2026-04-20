0f5975360c5d504554bb83515dc43dfef8b60259
yoga <yoga@yogas-MacBook-Pro.local>
2026-04-20 17:20:33 +0700

    fix login page

---
+++ src/lib/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.PUBLIC_API_URL;
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+// 🔥 Auto attach token
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default api;
